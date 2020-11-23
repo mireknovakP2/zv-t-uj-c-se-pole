9462f460cf6a6a380aa7c5ab2b0e22b58772c257
Miroslav Novák <u19.417@pslib.cloud>
2020-11-23 16:44:46 +0100

    Přidejte soubory projektu.

---
+++ zvětšující se pole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace zvětšující_se_pole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
